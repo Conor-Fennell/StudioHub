f5428220bef0bbf14d07b0347b53ad6065192dd8
Conor Fennell <64708631+Conor-Fennell@users.noreply.github.com>
2020-05-24 04:26:49 +0100

    Latest Version 24/05/2020
    Some minor bug fixes

--- Drive.py
+++ Drive.py
@@ -40,7 +40,7 @@ def GoogleAuth(creds):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'Library/client_id.json', SCOPES)
+                'client_id.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
